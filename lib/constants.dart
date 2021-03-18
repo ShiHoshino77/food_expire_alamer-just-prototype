cff333a3db2d348f486b0ffbc92e6d249949dd80
ShiHoshino77 <80907777+ShiHoshino77@users.noreply.github.com>
2021-03-18 22:07:06 +0700

    Initial commit

---
+++ lib/constants.dart
@@ -0,0 +1,64 @@
+const FOOD_DATA = [
+  {
+    "name":"Cabbage",
+    "Counter":"Counter 1-2 Days",
+    "Refrigerator":"Refrigerator 4-5 Weeks",
+    "Freezer":"Freezer 9-12 Months",
+    "image":"Cabbage.png"
+  },{
+    "name":"Apples",
+    "Counter":"Counter 2-4 Weeks",
+    "Refrigerator":"Refrigerator 1-2 Months",
+    "Freezer":"Freezer 9-12 Months",
+    "image":"Apples.png"
+  },
+  {
+    "name":"Fresh Chicken",
+    "Counter":"Counter 1 Days",
+    "Refrigerator":"Refrigerator 1-2 Days",
+    "Freezer":"Freezer 1 Year",
+    "image":"Chicken.png"
+  },
+  {
+    "name":"Fresh Carrots",
+    "Counter":"Counter 1-2 Days",
+    "Refrigerator":"Refrigerator 4-5 Weeks",
+    "Freezer":"Freezer 1 Year",
+    "image":"Carrots.png"
+  },
+  {
+    "name":"Fresh Bananas",
+    "Counter":"Counter 2-7 Days",
+    "Refrigerator":"Refrigerator 2-9 Days",
+    "Freezer":"Freezer 2-3 Months",
+    "image":"Bananas.png"
+  },
+  {
+    "name":"Pork Chops",
+    "Counter":"Counter 1 Days",
+    "Refrigerator":"Refrigerator 1-2 Days",
+    "Freezer":"Freezer 6-8 Months",
+    "image":"Pork.png"
+  },
+  {
+    "name":"Onions",
+    "Counter":"Counter 4-6 Weeks",
+    "Refrigerator":"Refrigerator 1-2 Months",
+    "Freezer":"Freezer 6-8 Months",
+    "image":"Onions.png"
+  },
+  {
+    "name":"Durian",
+    "Counter":"Counter 1-2 Days",
+    "Refrigerator":"Refrigerator4-5 Days",
+    "Freezer":"Freezer 2 Months",
+    "image":"Durian.png"
+  },
+  {
+    "name":"Fresh Beef",
+    "Counter":"Counter 1 Days",
+    "Refrigerator":"Refrigerator1-2 Days",
+    "Freezer":"Freezer 6-8 Months",
+    "image":"Beef.png"
+  }
+];
